579ac4470b49c6571b2deab168ed295b83eded69
Barbora Koutníková <koutnikovabar@gmail.com>
2022-12-27 11:02:34 +0100

    Added my-app  project to GitHub

---
+++ src/robots.js
@@ -0,0 +1,52 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Jack',
+        email: 'jack@gmail.com'
+    },
+    {
+        id: 2,
+        name: 'Adam',
+        email: 'adam@gmail.com'
+    },
+    {
+        id: 3,
+        name: 'John',
+        email: 'john@gmail.com'
+    },
+    {
+        id: 4,
+        name: 'Ted',
+        email: 'ted@gmail.com'
+    },
+    {
+        id: 5,
+        name: 'Tony',
+        email: 'tony@gmail.com'
+    },
+    {
+        id: 6,
+        name: 'Charles',
+        email: 'charles@gmail.com'
+    },
+    {
+        id: 7,
+        name: 'Joey',
+        email: 'joey@gmail.com'
+    },
+    {
+        id: 8,
+        name: 'Ross',
+        email: 'ross@gmail.com'
+    },
+    {
+        id: 9,
+        name: 'Chandler',
+        email: 'chandler@gmail.com'
+    },
+    {
+        id: 10,
+        name: 'Harry',
+        email: 'harry@gmail.com'
+    }
+]
